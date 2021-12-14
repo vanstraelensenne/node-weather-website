43ffd7f61792829814e9dad58c44dd08ea169870
Senne <vanstraelensenne@gmail.com>
2021-12-14 21:19:52 +0100

    Remove console.log statements which were not neccesary

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-console.log('Client side javascript file is loaded.')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
-
 const searchValue = document.querySelector('input')
 const weatherForm = document.querySelector('form')
 const messageOne = document.querySelector('#message-1')
